7d9243bbe7fe7429b49961c180dbe2b1765f7889
bergi <bergi@axolotlfarm.org>
2015-03-03 21:44:08 +0100

    changed base to <http://localhost:8080/> and create single graph N-Triples

--- prepublish.js
+++ prepublish.js
@@ -1,5 +1,48 @@
-// build N-Triples single graph
-//TODO
+var
+  fs = require('fs'),
+  glob = require('glob'),
+  path = require('path'),
+  rdf = require('rdf-interfaces'),
+  Promise = require('es6-promise').Promise;
+
+require('rdf-ext')(rdf);
+
+var
+  files,
+  graphs = {},
+  singleGraph = rdf.createGraph(),
+  parser = new rdf.promise.Parser(new rdf.TurtleParser());
+
+
+files = glob.sync('data/**/*.ttl').map(function (filename) {
+  var iri = 'http://localhost:8080/' +
+    path.dirname(filename) + '/'+
+    path.basename(filename, path.extname(filename));
+
+  return {filename: filename, iri: iri};
+});
+
+
+Promise.all(
+  files.map(function (file) {
+    return parser.parse(fs.readFileSync(file.filename).toString(), file.iri)
+      .then(function (graph) {
+        return graphs[file.iri] = graph;
+      })})
+  )
+  .then(function (result) {
+    result.forEach(function (graph) {
+      singleGraph.addAll(graph);
+    });
+  })
+  .then(function () {
+    // build N-Triples single graph
+    fs.writeFileSync('dist/tbbt.nt', singleGraph.toString());
+  })
+  .catch(function (error) {
+    console.error(error.stack);
+  });
+
 
 // build N-Quads
 //TODO
